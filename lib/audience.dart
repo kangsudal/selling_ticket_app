85d26becf84980a93e92d5b9e1acb92cf0f730ae
kangsudal_notebook <kangsudal@gmail.com>
2024-09-09 05:54:42 +0900

    bag의 구현(메서드들) 캡슐화

--- lib/audience.dart
+++ lib/audience.dart
@@ -8,20 +8,9 @@ class Audience{
 
   int buy(Ticket ticket){
     //buy 메서드는 인자로 전달된 Ticket을 Bag에 넣은 후 지불된 금액을 반환한다.
-    if (_bag.hasInvitation()) {
-      //초대장이 있다면 돈 안내고 가방의 돈을 지킨채 ㅋㅋ 공짜로 티켓을 얻는다.
-      //티켓을 얻었다고 얻은 이후의 내용은 따로 없는듯.
-      //null로 설정돼있는(티켓이 없는) 상태에서 매표소에서 얻은 ticket을 쥐어준다.
-      _bag.setTicket(ticket);
-      return 0;
-      //질문5: Invitation을 삭제시키는 작업이 필요할것같다. 한번 얻은거로 그날 종일 패스권이 아니니깐
-    } else {
-      //불행히도 초대장이 없는 관객은 가방에 돈이 줄어드는 코드가 들어간다.
-      _bag.minusAmount(ticket.getFee());
-      _bag.setTicket(ticket);
-      //가방에서 뺏은 돈을 매표소 매출로 옮겨넣는다.
-      // _ticketOffice.plusAmount(ticket.getFee());
-      return ticket.getFee();
+    return _bag.hold(ticket);
+    // bag에 ticket의 상태를 설정해준다.
+    // bag에서 뺏은 돈을 매표소로 전달하기위해 반환해준다.
     }
   }
 }
--- lib/bag.dart
+++ lib/bag.dart
@@ -34,4 +34,21 @@ class Bag {
   void plusAmount(int amount) {
     _amount += amount;
   }
+
+  int hold(Ticket ticket){
+    if (hasInvitation()) {
+      //초대장이 있다면 돈 안내고 가방의 돈을 지킨채 ㅋㅋ 공짜로 티켓을 얻는다.
+      //티켓을 얻었다고 얻은 이후의 내용은 따로 없는듯.
+      //null로 설정돼있는(티켓이 없는) 상태에서 매표소에서 얻은 ticket을 쥐어준다.
+      setTicket(ticket);
+      return 0;
+      //질문5: Invitation을 삭제시키는 작업이 필요할것같다. 한번 얻은거로 그날 종일 패스권이 아니니깐
+    } else {
+      //불행히도 초대장이 없는 관객은 가방에 돈이 줄어드는 코드가 들어간다.
+      minusAmount(ticket.getFee());
+      setTicket(ticket);
+      //가방에서 뺏은 돈을 매표소 매출로 옮겨넣기위해 티켓비를 반환한다.
+      // _ticketOffice.plusAmount(ticket.getFee());
+      return ticket.getFee();
+  }
 }
